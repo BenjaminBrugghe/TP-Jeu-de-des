b4b1e516671b8b3d43b2ee3d4ae2b7d02e7199a1
BenjaminBrugghe <Brugghe.b@gmail.com>
2022-12-01 16:32:36 +0100

    Refactor: refactorisation de la classe Des :recycle:

--- Classes/Des.ts
+++ Classes/Des.ts
@@ -2,18 +2,24 @@ class Des {
   //Attributs
   private _valeur!: number;
 
+  // Getter
+  public get valeur(): number {
+    return this._valeur;
+  }
+
+  // Setter
+  public set valeur(nouvelleValeur: number) {
+    this._valeur = nouvelleValeur;
+  }
+
   // Méthodes
   /**
    * Génère un nombre aléatoire entre 1 et 6 (dé standard).
    * @returns un nombre entre 1 et 6.
    */
   public lancer(): number {
-    return Math.floor(Math.random() * (6 - 1) + 1);
-  }
-
-  // Getter
-  public get valeurDuDe(): number {
-    return this.lancer();
+    this._valeur = Math.floor(Math.random() * (6 - 1) + 1);
+    return this._valeur;
   }
 }
 
